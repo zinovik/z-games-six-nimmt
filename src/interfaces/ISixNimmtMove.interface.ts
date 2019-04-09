3840314e333a960b5376c6b335b73343a1a35ec9
Max Zinovik <zinovik@gmail.com>
2019-04-09 17:57:36 +0300

    update game move

--- src/interfaces/ISixNimmtMove.interface.ts
+++ src/interfaces/ISixNimmtMove.interface.ts
@@ -2,6 +2,12 @@ import { IBaseGameMove } from 'z-games-base-game';
 
 import { ISixNimmtCard } from './';
 
-export interface ISixNimmtMove extends IBaseGameMove {
+export interface ISixNimmtCardMove extends IBaseGameMove {
   card: ISixNimmtCard;
 }
+
+export interface ISixNimmtRowNumberMove extends IBaseGameMove {
+  rowNumber: number;
+}
+
+export declare type ISixNimmtMove = ISixNimmtCardMove | ISixNimmtRowNumberMove;
